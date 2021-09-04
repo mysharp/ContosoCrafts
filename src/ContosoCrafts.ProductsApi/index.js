84a61a0dbdb6d8672817d8a8f043af3466387955
cecilphillip <cecilphillip@gmail.com>
2021-09-04 01:41:33 -0400

    use env configs

--- src/ContosoCrafts.ProductsApi/index.js
+++ src/ContosoCrafts.ProductsApi/index.js
@@ -4,14 +4,15 @@ import productRouter from './product.router.js';
 import mongoose from 'mongoose';
 
 const app = express();
-const app_port = process.env.APP_PORT || 3000
+const app_port = process.env.NODE_APP_PORT || 80
+const mongodbHost = process.env.NODE_ENV == 'production' ? 'mongo_service:27017' : 'localhost:27017'
 
 app.use(express.json())
 
 app.use('/products', productRouter);
 
-try {
-    await mongoose.connect('mongodb://admin:admin@localhost:27017', 
+try {    
+    await mongoose.connect(`mongodb://admin:admin@${mongodbHost}`,
         { dbName: 'contosocrafts', serverSelectionTimeoutMS: 5000 }
     );
 
@@ -20,6 +21,8 @@ try {
             logger.error(error, "oh oh.. something bad happened");
             throw new Error(error);
         }
+
+        logger.info(`Node Environment ${process.env.NODE_ENV}`);
         logger.info(`Listening on port ${app_port}`);
     });
 
--- src/ContosoCrafts.ProductsApi/logger.js
+++ src/ContosoCrafts.ProductsApi/logger.js
@@ -1,21 +1,23 @@
 import { createLogger } from 'bunyan';
-import bunyan_seq from 'bunyan-seq';
-const { createStream } = bunyan_seq;
+import bunyanSeq from 'bunyan-seq';
 
-let logger = createLogger({
-    name: 'myapp',
-    streams: [
-        {
-            name: 'console',
-            stream: process.stdout,
-            level: 'info',
-        },
-        createStream({
-            name: 'seq',
-            serverUrl: 'http://localhost:8191',
-            level: 'info'
-        })
-    ]
+const { createStream } = bunyanSeq;
+
+const seqUrl = process.env.NODE_ENV === 'production' ? 'http://seq_service:80' : 'http://localhost:8191';
+const logger = createLogger({
+  name: 'myapp',
+  streams: [
+    {
+      name: 'console',
+      stream: process.stdout,
+      level: 'info',
+    },
+    createStream({
+      name: 'seq',
+      serverUrl: seqUrl,
+      level: 'info',
+    }),
+  ],
 });
 
 export default logger;
